739814388bc5ff78fb7beedcd426aea1162f33f5
unknown <freitasandre38@gmail.com>
2021-10-12 23:20:51 -0300

    Página visual

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -5,15 +5,16 @@ import About from "./Pages/About/About";
 import FullMap from "./Pages/Map/MapContainer";
 import Project from './Pages/Project/Project';
 import Skills from "./Pages/Skills/Skills";
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const Routes = () =>(
-    <Switch>
+    <Router basename={process.env.PUBLIC_URL}>
         <Route exact path="/" component={Home}/>
         <Route exact path="/sobre" component={About}/>
         <Route exact path="/mapa" component={FullMap}/>
         <Route exact path="/projeto" component={Project}/>
         <Route exact path="/habilidades" component={Skills}/>
         <Redirect from='*' to='/'/>
-    </Switch>
+    </Router>
 )
 export default Routes
